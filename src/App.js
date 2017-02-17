a5f45b0503fd8106530e3b74e2797796ac6d41c1
Jayan Zaman <jayanzaman@gmail.com>
2017-02-17 10:42:01 -0500

    added search box

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+injectTapEventPlugin();
 import './App.css';
+import SearchBox from './SearchBox'
+import makeExpanding from './expanding-animation';
+
+const ExpandingSearchBox = makeExpanding(SearchBox);
 
 class App extends Component {
   render() {
+        //https://css-tricks.com/quick-css-trick-how-to-center-an-object-exactly-in-the-center/
+        const style = {
+            position: 'fixed',
+            top: '50%',
+            left: '50%',
+            transform: 'translate(-50%, -50%)',
+        };
+
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+        <MuiThemeProvider>
+          <div style={style}>
+            <ExpandingSearchBox/>
+          </div>
+        </MuiThemeProvider>
     );
   }
 }
